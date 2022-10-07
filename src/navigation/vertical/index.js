953bc1ff2e94df0d2c38c56275774bfaacc18490
lesnikutsa <35654095+lesnikutsa@users.noreply.github.com>
2022-10-07 15:18:49 +0300

    Update index.js

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -14,29 +14,47 @@ function processMenu() {
   })
 
   if (blockchains.length > 1) {
-    chainMenus.push({ header: 'CHAINS' })
+    chainMenus.push({ header: 'ecosystem' })
     chainMenus.push({
       title: 'blockchains',
       children: blockchains,
       tag: `${blockchains.length}`,
-      icon: 'https://github.com/lesnikutsa/lesnik_utsa/blob/main/logo/logo.svg',
+      icon: 'https://ping.pub/logo.svg',
     })
   }
   chainMenus.push({ header: 'LINKS' })
+  if (isTestnet()) {
     chainMenus.push({
-    title: 'Blog lesnik 🌲 UTSA',
-    href: 'https://teletype.in/@lesnik13utsa',
-    icon: 'ChromeIcon',
-  })
+      title: 'Mainnet Explorer',
+      href: 'https://ping.pub',
+      icon: 'ChromeIcon',
+    })
+  } else {
+    chainMenus.push({
+      title: 'Testnet Explorer',
+      href: 'http://testnet.ping.pub',
+      icon: 'LifeBuoyIcon',
+    })
+  }
   chainMenus.push({
     title: 'Github',
-    href: 'https://github.com/lesnikutsa',
+    href: 'https://github.com/ping-pub/explorer',
     icon: 'GithubIcon',
   })
   chainMenus.push({
     title: 'Discord',
-    href: 'lesnik_utsa#4480',
+    href: 'https://discord.gg/CmjYVSr6GW',
     icon: 'EyeIcon',
+  })
+  chainMenus.push({
+    title: 'Twitter',
+    href: 'https://twitter.com/ping_pub',
+    icon: 'TwitterIcon',
+  })
+  chainMenus.push({
+    title: 'Telegram',
+    href: 'https://t.me/pingpub',
+    icon: 'SendIcon',
   })
 
   return chainMenus
