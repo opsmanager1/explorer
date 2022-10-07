60215422786bfa079f009bdfce33b63034afdce4
lesnikutsa <35654095+lesnikutsa@users.noreply.github.com>
2022-10-07 18:58:37 +0300

    Update Staking.vue

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card
       v-if="pingVals && pingVals.length > 0"
-      title="🌲🌲🌲 To plant new trees 🌲🌲🌲"
+      title="🌲 To plant new trees 🌲"
       class="overflow-auto"
     >
       <b-table
@@ -341,7 +341,7 @@ export default {
   },
   computed: {
     pingVals() {
-      return this.list.filter(x => x.description.identity === '6783E9F948541962')
+      return this.list.filter(x => x.description.identity === 'A0B5FF00B5460153')
     },
     list() {
       const tab = this.selectedStatus === 'active' ? this.validators : this.inactiveValidators
